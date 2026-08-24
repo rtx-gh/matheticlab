aa6a61c55741342550b74982b1011cb8f25aaa05
rtx-gh <235mpwptdamgtm@gmail.com>
2026-08-25 00:45:41 +0900

    各種変更

---
+++ src/components/math-art/system/registry.ts
@@ -0,0 +1,11 @@
+import Hypotrochoid from "../shapes/Hypotrochoid.astro";
+import Epitrochoid from "../shapes/Epitrochoid.astro";
+import Lissajous from "../shapes/Lissajous.astro";
+import RoseCurve from "../shapes/RoseCurve.astro";
+
+export const mathArtShapes = [
+  Hypotrochoid,
+  Epitrochoid,
+  Lissajous,
+  RoseCurve,
+];
